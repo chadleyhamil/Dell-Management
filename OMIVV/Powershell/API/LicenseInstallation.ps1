dddb0e14f0eea5ca7ec69754f8fc9558c3b17833
Chadley Hamil <chadley_hamil@dell.com>
2021-08-13 08:57:30 -0500

    Updated license script

--- OMIVV/Powershell/API/LicenseInstallation.ps1
+++ OMIVV/Powershell/API/LicenseInstallation.ps1
@@ -8,34 +8,71 @@
 #########################################################################################################
 
 #IP address or FQDN of the OMIVV instance
-$omivv = "omivvfqdnip"
+$omivv = "<OMIVV Server>"
 
 #Share Info
 $type = "CIFS"
-$path = "\\\\hostnameip\\folder\\folder\\"
-$licenses = "\\hostnameip\folder\folder\"
+$licensespath = "\\<server>\<folder>\"
+
 
 #Share Creds
-$username = "user"
-$password = "password"
-$domain = "domain"
+$credential = Get-Credential -Message "<Domain>\<Username> Formatted credentials for CIFS Share"
 
-foreach($l in (Get-ChildItem -Path $licenses).name){
-$headers = New-Object "System.Collections.Generic.Dictionary[[String],[String]]"
-$headers.Add("Authorization", "Bearer $bearertoken")
-$headers.Add("Content-Type", "application/json")
+#OMIVV Creds
+$OMIAdminusername = "admin"
+$OMIAdminpassword = "<password>"
 
-$body = "{
-`n`"sharetype`": `"$type`",
-`n`"path`": `"$path$l`",
-`n`"credential`": {
-`n    `"username`": `"$username`",
-`n    `"domain`": `"$domain`",
-`n    `"password`": `"$password`"
-`n    }
-`n}
-`n"
-Write-Host "Processing $l"
-$response = Invoke-webrequest -SkipCertificateCheck "https://$omivv/Spectre/api/rest/v1/Services/LicenseService/Licenses" -Method 'POST' -Headers $headers -Body $body
-$response.StatusDescription
+# Generate Token with admin credentials for OMIVV. For use with setting/adding licenses.
+
+# Generate the body for the Rest request. This will be converted to JSON after the object is generated
+$Tokenbody = @{
+    "apiUserCredential" = @{
+        "username" = $OMIAdminusername
+        "domain" = ""
+        "password" = $OMIAdminpassword
+    }
+} | ConvertTo-Json
+
+$TokenRest = @{
+    Uri = "https://$omivv/Spectre/api/rest/v1/Services/AuthenticationService/login"
+    Method = 'POST'
+    Body = $Tokenbody
+    HEADERS = @{
+        'Content-Type' = 'application/json'
+    }
+}
+
+$response = Invoke-RestMethod @TokenRest
+$bearertoken = $response.accessToken
+$bearertoken
+
+
+foreach($license in (Get-ChildItem -Path $licensespath)){
+    
+    # Create the body for the Rest request. This will be converted to JSON after the object is generated.
+    $Licensebody = @{
+    "sharetype" = $type
+    "path" = $license.FullName
+    "credential" = @{
+        "username" = ($credential.GetNetworkCredential().Username)
+        "domain" = ($credential.GetNetworkCredential().Domain)
+        "password" = ($credential.GetNetworkCredential().Password)
+        }
+    } | ConvertTo-Json
+
+    $LicenseRest = @{
+        Uri = "https://$omivv/Spectre/api/rest/v1/Services/LicenseService/Licenses"
+        Method = 'POST'
+        Body = $Licensebody
+        HEADERS = @{
+            'Authorization' = "Bearer $bearertoken"
+            'Content-Type' = 'application/json'
+        }
+    }
+    
+    $response = $null
+    Write-Host "Processing $($license.name)"
+    $response = try { Invoke-WebRequest @LicenseRest } catch {$_}
+    if ($response.ErrorDetails.Message) {Write-Error $response.ErrorDetails.Message}
+    $response.StatusDescription
 }
